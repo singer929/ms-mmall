b411a538d98d20a04d7649bf5c01984ae7d9438e
Administrator <Administrator@192.168.1.8>
2017-02-13 16:03:24 +0800

    运费模块基础框架提交

---
+++ src/main/java/com/mingsoft/freight/action/web/FreightAction.java
@@ -0,0 +1,44 @@
+/**
+The MIT License (MIT) * Copyright (c) 2016 铭飞科技(mingsoft.net)
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mingsoft.freight.action.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mingsoft.base.action.BaseAction;
+import com.mingsoft.freight.biz.IFreightBiz;
+import com.mingsoft.freight.dao.IFreightDao;
+import com.mingsoft.freight.entity.FreightEntity;
+
+@Controller()
+@RequestMapping("/${managerPath}/freight")
+public class FreightAction extends BaseAction {
+
+	/**
+	 * 注入用户基础业务层
+	 */
+	@Autowired
+	private IFreightBiz freightBiz;
+	
+	
+}
---
+++ src/main/java/com/mingsoft/freight/entity/FreightEntity.java
@@ -0,0 +1,75 @@
+/**
+The MIT License (MIT) * Copyright (c) 2016 铭飞科技(mingsoft.net)
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mingsoft.freight.entity;
+
+import java.util.Date;
+
+import com.mingsoft.base.entity.BaseEntity;
+
+
+public class FreightEntity extends BaseEntity {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private int peopleId;
+	
+	private String peopleName;
+	
+	private String peoplePassword;
+	
+	private Date peopleDateTime;
+
+	public int getPeopleId() {
+		return peopleId;
+	}
+
+	public void setPeopleId(int peopleId) {
+		this.peopleId = peopleId;
+	}
+
+	public String getPeopleName() {
+		return peopleName;
+	}
+
+	public void setPeopleName(String peopleName) {
+		this.peopleName = peopleName;
+	}
+
+	public String getPeoplePassword() {
+		return peoplePassword;
+	}
+
+	public void setPeoplePassword(String peoplePassword) {
+		this.peoplePassword = peoplePassword;
+	}
+
+	public Date getPeopleDateTime() {
+		return peopleDateTime;
+	}
+
+	public void setPeopleDateTime(Date peopleDateTime) {
+		this.peopleDateTime = peopleDateTime;
+	}
+}
