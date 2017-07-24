11701aa312fcef8e71dcc4e3d984d9a1d48bed9c
ww <ww@wjj>
2017-07-25 00:38:16 +0800

    订单评论提交

--- src/main/java/net/mingsoft/mall/action/OrderCommentAction.java
+++ src/main/java/net/mingsoft/mall/action/OrderCommentAction.java
@@ -13,22 +13,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import net.mingsoft.mall.biz.IOrderCommentBiz;
 import net.mingsoft.mall.entity.OrderCommentEntity;
+import net.mingsoft.base.util.JSONObject;
 import com.mingsoft.util.PageUtil;
 import com.mingsoft.util.StringUtil;
+import com.mingsoft.base.entity.BaseEntity;
 import net.mingsoft.basic.util.BasicUtil;
+import net.mingsoft.comment.biz.ICommentBiz;
+import net.mingsoft.basic.bean.ListBean;
+import com.mingsoft.base.filter.DateValueFilter;
+import com.mingsoft.base.filter.DoubleValueFilter;
+import net.mingsoft.basic.bean.EUListBean;
 	
 /**
  * 订单评价管理控制层
- * @author 铭飞开发团队
+ * @author 伍晶晶
  * @version 
- * 版本号：1.0.0<br/>
- * 创建日期：<br/>
+ * 版本号：100<br/>
+ * 创建日期：2017-7-22 11:12:39<br/>
  * 历史修订：<br/>
  */
 @Controller
@@ -41,143 +49,51 @@ public class OrderCommentAction extends net.mingsoft.mall.action.BaseAction{
 	@Autowired
 	private IOrderCommentBiz orderCommentBiz;
 	
+	/**
+	 * 注入评价业务层
+	 */	
+	@Autowired
+	private ICommentBiz commentBiz;
+	
+	
+	/**
+	 * 返回主界面index
+	 */
+	@RequestMapping("/index")
+	public String index(HttpServletResponse response,HttpServletRequest request){
+		return view ("/mall/order_comment/index");
+	}
 	
 	/**
 	 * 查询订单评价列表
-	 * @param response
-	 * @param request
-	 * @param model
+	 * @param orderComment 订单评价实体
+	 * <i>orderComment参数包含字段信息参考：</i><br/>
+	 * ocCommentId 评论编号<br/>
+	 * ocOrderId 订单编号<br/>
+	 * ocImpression 印象<br/>
+	 * ocProduct 商品符合度<br/>
+	 * ocService 店家服务态度<br/>
+	 * ocLogistics 物流发货速度<br/>
+	 * ocServiceDescribe 评价服务<br/>
+	 * <dt><span class="strong">返回</span></dt><br/>
+	 * <dd>[<br/>
+	 * { <br/>
+	 * ocCommentId: 评论编号<br/>
+	 * ocOrderId: 订单编号<br/>
+	 * ocImpression: 印象<br/>
+	 * ocProduct: 商品符合度<br/>
+	 * ocService: 店家服务态度<br/>
+	 * ocLogistics: 物流发货速度<br/>
+	 * ocServiceDescribe: 评价服务<br/>
+	 * }<br/>
+	 * ]</dd><br/>	 
 	 */
 	@RequestMapping("/list")
-	public String list(@ModelAttribute OrderCommentEntity orderComment,HttpServletResponse response, HttpServletRequest request,ModelMap model) {
+	@ResponseBody
+	public void list(@ModelAttribute OrderCommentEntity orderComment,HttpServletResponse response, HttpServletRequest request,ModelMap model) {
 		BasicUtil.startPage();
-		List<?> orderCommentList = orderCommentBiz.query(orderComment);
-		BasicUtil.endPage(orderCommentList);
-		model.addAttribute("orderCommentList", orderCommentList);	
-		return view("/mall/order_comment/order_comment_list");
-	}
-	
-	/**
-	 * 新增 订单评价
-	 * @param orderComment 订单评价实体
-	 * <i>orderComment参数包含字段信息参考：</i><br/>
-	 * ocCommentId 评论编号<br/>
-	 * ocOrderId 订单编号<br/>
-	 * ocImpression 印象<br/>
-	 * ocProduct 商品符合度<br/>
-	 * ocService 店家服务态度<br/>
-	 * ocLogistics 物流发货速度<br/>
-	 * ocServiceDescribe 评价服务<br/>
-	 * @param response
-	 * @param request
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping("/add")
-	public String add(@ModelAttribute OrderCommentEntity orderComment,HttpServletResponse response, HttpServletRequest request,ModelMap model){
-		model.addAttribute("orderComment", orderComment);		
-		return view("/mall/order_comment/order_comment_form");
-	}
-	
-	/**
-	 * 编辑
-	 * @param response
-	 * @param request
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping("/edit")
-	public String edit(@ModelAttribute OrderCommentEntity orderComment,HttpServletResponse response, HttpServletRequest request,ModelMap model){
-		if(orderComment.getOcCommentId()<=0) {
-			this.outJson(response, null, false, getResString("err.error", this.getResString("oc.comment.id")));
-		}
-		OrderCommentEntity _orderComment = (OrderCommentEntity)orderCommentBiz.getEntity(orderComment.getOcCommentId());
-		model.addAttribute("orderComment", _orderComment);		
-		BasicUtil.removeUrlParams(new String[]{"ocCommentId"});
-		return view("/mall/order_comment/order_comment_form");
-	}
-	
-	/**
-	 * 保存订单评价实体
-	 * @param orderComment 订单评价实体
-	 * <i>orderComment参数包含字段信息参考：</i><br/>
-	 * ocCommentId 评论编号<br/>
-	 * ocOrderId 订单编号<br/>
-	 * ocImpression 印象<br/>
-	 * ocProduct 商品符合度<br/>
-	 * ocService 店家服务态度<br/>
-	 * ocLogistics 物流发货速度<br/>
-	 * ocServiceDescribe 评价服务<br/>
-	 * @param response
-	 * @param request
-	 */
-	@PostMapping("/save")
-	@ResponseBody
-	public void save(@ModelAttribute OrderCommentEntity orderComment, HttpServletResponse response, HttpServletRequest request) {
-		//验证印象的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcImpression())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.impression")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcImpression()+"", 1, 255)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.impression"), "1", "255"));
-			return;			
-		}
-		//验证商品符合度的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcProduct())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.product")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcProduct()+"", 1, 10)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.product"), "1", "10"));
-			return;			
-		}
-		//验证店家服务态度的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcService())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.service")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcService()+"", 1, 10)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.service"), "1", "10"));
-			return;			
-		}
-		//验证物流发货速度的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcLogistics())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.logistics")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcLogistics()+"", 1, 10)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.logistics"), "1", "10"));
-			return;			
-		}
-		//验证评价服务的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcServiceDescribe())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.service.describe")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcServiceDescribe()+"", 1, 255)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.service.describe"), "1", "255"));
-			return;			
-		}
-		orderCommentBiz.saveEntity(orderComment);
-		this.outJson(response, true);
-	}
-
-	/**
-	 * 批量删除订单评价
-	 * @param response
-	 * @param request
-	 */
-	@RequestMapping("/delete")
-	@ResponseBody
-	public void delete(HttpServletResponse response, HttpServletRequest request) {
-		int[] ids = BasicUtil.getInts("ocCommentId");
-		if(ids==null){
-			this.outJson(response,null, false);
-			return;
-		}
-		orderCommentBiz.delete(ids);		
-		this.outJson(response, true);
+		List orderCommentList = orderCommentBiz.query(orderComment);
+		this.outJson(response, net.mingsoft.base.util.JSONArray.toJSONString(new EUListBean(orderCommentList,(int)BasicUtil.endPage(orderCommentList).getTotal()),new DoubleValueFilter(),new DateValueFilter()));
 	}
 	
 	/** 
@@ -191,61 +107,75 @@ public class OrderCommentAction extends net.mingsoft.mall.action.BaseAction{
 	 * ocService 店家服务态度<br/>
 	 * ocLogistics 物流发货速度<br/>
 	 * ocServiceDescribe 评价服务<br/>
-	 * @param response
-	 * @param request
+	 * <dt><span class="strong">返回</span></dt><br/>
+	 * <dd>{ <br/>
+	 * ocCommentId: 评论编号<br/>
+	 * ocOrderId: 订单编号<br/>
+	 * ocImpression: 印象<br/>
+	 * ocProduct: 商品符合度<br/>
+	 * ocService: 店家服务态度<br/>
+	 * ocLogistics: 物流发货速度<br/>
+	 * ocServiceDescribe: 评价服务<br/>
+	 * }</dd><br/>
 	 */
 	@PostMapping("/update")
 	@ResponseBody	 
-	public void update(@ModelAttribute OrderCommentEntity orderComment, HttpServletResponse response,
+	public void update(@RequestBody List<OrderCommentEntity> orderComments, HttpServletResponse response,
 			HttpServletRequest request) {
-		//验证印象的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcImpression())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.impression")));
-			return;			
+		if(orderComments.size() > 0){
+			for(int i = 0;i<orderComments.size();i++){
+				//验证印象的值是否合法			
+				if(StringUtil.isBlank(orderComments.get(i).getOcImpression())){
+					this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.impression")));
+					return;			
+				}
+				if(!StringUtil.checkLength(orderComments.get(i).getOcImpression()+"", 1, 255)){
+					this.outJson(response, null, false, getResString("err.length", this.getResString("oc.impression"), "1", "255"));
+					return;			
+				}
+				//验证商品符合度的值是否合法			
+				if(StringUtil.isBlank(orderComments.get(i).getOcProduct())){
+					this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.product")));
+					return;			
+				}
+				if(!StringUtil.checkLength(orderComments.get(i).getOcProduct()+"", 1, 10)){
+					this.outJson(response, null, false, getResString("err.length", this.getResString("oc.product"), "1", "10"));
+					return;			
+				}
+				//验证店家服务态度的值是否合法			
+				if(StringUtil.isBlank(orderComments.get(i).getOcService())){
+					this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.service")));
+					return;			
+				}
+				if(!StringUtil.checkLength(orderComments.get(i).getOcService()+"", 1, 10)){
+					this.outJson(response, null, false, getResString("err.length", this.getResString("oc.service"), "1", "10"));
+					return;			
+				}
+				//验证物流发货速度的值是否合法			
+				if(StringUtil.isBlank(orderComments.get(i).getOcLogistics())){
+					this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.logistics")));
+					return;			
+				}
+				if(!StringUtil.checkLength(orderComments.get(i).getOcLogistics()+"", 1, 10)){
+					this.outJson(response, null, false, getResString("err.length", this.getResString("oc.logistics"), "1", "10"));
+					return;			
+				}
+				//验证评价服务的值是否合法			
+				if(StringUtil.isBlank(orderComments.get(i).getOcServiceDescribe())){
+					this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.service.describe")));
+					return;			
+				}
+				if(!StringUtil.checkLength(orderComments.get(i).getOcServiceDescribe()+"", 1, 255)){
+					this.outJson(response, null, false, getResString("err.length", this.getResString("oc.service.describe"), "1", "255"));
+					return;			
+				}
+				orderComments.get(i).setCommentAudit(1);
+				commentBiz.updateEntity(orderComments.get(i));
+			}
+			this.outJson(response, true);
+		}else{
+			this.outJson(response, false);
 		}
-		if(!StringUtil.checkLength(orderComment.getOcImpression()+"", 1, 255)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.impression"), "1", "255"));
-			return;			
-		}
-		//验证商品符合度的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcProduct())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.product")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcProduct()+"", 1, 10)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.product"), "1", "10"));
-			return;			
-		}
-		//验证店家服务态度的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcService())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.service")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcService()+"", 1, 10)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.service"), "1", "10"));
-			return;			
-		}
-		//验证物流发货速度的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcLogistics())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.logistics")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcLogistics()+"", 1, 10)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.logistics"), "1", "10"));
-			return;			
-		}
-		//验证评价服务的值是否合法			
-		if(StringUtil.isBlank(orderComment.getOcServiceDescribe())){
-			this.outJson(response, null,false,getResString("err.empty", this.getResString("oc.service.describe")));
-			return;			
-		}
-		if(!StringUtil.checkLength(orderComment.getOcServiceDescribe()+"", 1, 255)){
-			this.outJson(response, null, false, getResString("err.length", this.getResString("oc.service.describe"), "1", "255"));
-			return;			
-		}
-		orderCommentBiz.updateEntity(orderComment);
-		this.outJson(response, true);
 	}
-	
 		
 }
---
+++ src/main/webapp/WEB-INF/manager/mall/order_comment/index.ftl
@@ -0,0 +1,127 @@
+<@ms.html5>
+	<@ms.nav title="订单评价管理"></@ms.nav>
+	<@ms.searchForm name="searchForm" isvalidation=true>
+			<@ms.searchFormButton>
+				<@ms.select 
+	 				default="未审核"
+	 				value="0"
+				    name="commentAudit" 
+				    label="审核状态" 
+				    list=[{"id":1,"value":"审核通过"},{"id":2,"value":"审核不通过"}]
+				    listKey="id" 
+				    listValue="value"
+				/>
+				<@ms.text name="basicTitle" label="商品名称"/>
+				 <@ms.queryButton onclick="search()"/> 
+			</@ms.searchFormButton>			
+	</@ms.searchForm>
+	<@ms.panel>
+		<div id="toolbar">
+			<@ms.panelNav>
+				<@ms.button id="audit" value="审核"/>
+			</@ms.panelNav>
+		</div>
+		<table id="orderCommentList" 
+			data-show-refresh="true"
+			data-show-columns="true"
+			data-show-export="true"
+			data-method="post" 
+			data-pagination="true"
+			data-page-size="10"
+			data-side-pagination="server">
+		</table>
+	</@ms.panel>
+	
+	<@ms.modal  modalName="auditOrderComment" title="通过审查" >
+		<@ms.modalBody>是否通过选中记录
+			<@ms.modalButton>
+				<!--模态框按钮组-->
+				<@ms.button  value="确认通过？"  id="auditOrderCommentBtn"  />
+			</@ms.modalButton>
+		</@ms.modalBody>
+	</@ms.modal>
+</@ms.html5>
+
+<script>
+	$(function(){
+		$("#orderCommentList").bootstrapTable({
+			url:"${managerPath}/mall/orderComment/list.do",
+			contentType : "application/x-www-form-urlencoded",
+			queryParamsType : "undefined",
+			toolbar: "#toolbar",
+	    	columns: [{ checkbox: true},
+				    	{
+				        	field: 'basicTitle',
+				        	title: '商品名称'
+				    	},{
+				        	field: 'ocImpression',
+				        	title: '印象',
+				        	align: 'center'
+				    	},{
+				        	field: 'ocProduct',
+				        	title: '商品符合度',
+				        	align: 'center'
+				    	},{
+				        	field: 'commentContent',
+				        	title: '评价内容'
+				    	},{
+				        	field: 'commentPeopleUser.peopleUserNickName',
+				        	title: '用户昵称'
+				    	},{
+				        	field: 'commentAudit',
+				        	title: '审核状态',
+				        	formatter: function (value, row, index){
+				        		if(value == 0){
+				        			return "未审核"
+				        		}else if(value == 1){
+				        			return "审核通过"
+				        		}else{
+				        			return "审核不通过"
+				        		}
+				        	}
+				    	}]
+	    })
+	})
+	//审查按钮
+	$("#audit").click(function(){
+		//获取checkbox选中的数据
+		var rows = $("#orderCommentList").bootstrapTable("getSelections");
+		//没有选中checkbox
+		if(rows.length <= 0){
+			<@ms.notify msg="请选择需要审查的记录" type="warning"/>
+		}else{
+			$(".auditOrderComment").modal();
+		}
+	})
+	
+	$("#auditOrderCommentBtn").click(function(){
+		var rows = $("#orderCommentList").bootstrapTable("getSelections");
+		$(this).text("正在审核...");
+		$(this).attr("disabled","true");
+		$.ajax({
+			type: "post",
+			url: "${managerPath}/mall/orderComment/update.do",
+			data: JSON.stringify(rows),
+			dataType: "json",
+			contentType: "application/json",
+			success:function(msg) {
+				if(msg.result == true) {
+					<@ms.notify msg= "审核成功" type= "success" />
+				}else {
+					<@ms.notify msg= "审核失败" type= "fail" />
+				}
+				location.reload();
+			}
+		})
+	});
+	//查询功能
+	function search(){
+		var search = $("form[name='searchForm']").serializeJSON();
+        var params = $('#orderCommentList').bootstrapTable('getOptions');
+        params.queryParams = function(params) {  
+        	$.extend(params,search);
+	        return params;  
+       	}  
+   	 	$("#orderCommentList").bootstrapTable('refresh', {query:$("form[name='searchForm']").serializeJSON()});
+	}
+</script>
