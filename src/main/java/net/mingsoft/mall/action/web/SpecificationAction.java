54488abd7b97b759d17333f4bbd1a0be030d07a4
ww <ww@wjj>
2017-08-10 18:43:40 +0800

    根据categoryId查询规格

--- src/main/java/net/mingsoft/mall/action/web/SpecificationAction.java
+++ src/main/java/net/mingsoft/mall/action/web/SpecificationAction.java
@@ -1,32 +1,24 @@
 package net.mingsoft.mall.action.web;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.ui.ModelMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.alibaba.fastjson.JSONArray;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
+
 import net.mingsoft.mall.biz.ISpecificationBiz;
 import net.mingsoft.mall.entity.SpecificationEntity;
-
-import com.mingsoft.base.entity.BaseEntity;
-import com.mingsoft.util.PageUtil;
-import com.mingsoft.util.StringUtil;
-import net.mingsoft.basic.util.BasicUtil;
 	
 /**
  * 默认规格数据管理控制层
@@ -62,7 +54,7 @@ public class SpecificationAction extends net.mingsoft.mall.action.BaseAction{
 	 * }<br/>
 	 * ]</dd><br/>	 
 	 */
-	@RequestMapping("/queryByCategoryId")
+	@PostMapping("/queryByCategoryId")
 	@ResponseBody
 	public void queryByCategoryId(@RequestParam int categoryId, HttpServletResponse response, HttpServletRequest request) {
 		if(categoryId <= 0){
@@ -71,12 +63,27 @@ public class SpecificationAction extends net.mingsoft.mall.action.BaseAction{
 		SpecificationEntity specification = new SpecificationEntity();
 		specification.setSpecificationCategoryId(categoryId);
 		List specificationList = specificationBiz.query(specification);
-//		for(int i = 0 ;i<specificationList.size();i++){
-//			BasicUtil.getIds(specificationList.get(i).)
-//		}
-//		Map DefaultField =new HashMap();
-//		DefaultField.put("specificationDefaultFields", value)
-		
+		List<SpecificationEntity> specificationEntityList = new ArrayList<SpecificationEntity>();
+		List<Map> List = new ArrayList<Map>();
+		for(int i = 0 ;i<specificationList.size();i++){
+			//转实体，获取规格名称
+			SpecificationEntity temp = (SpecificationEntity) specificationList.get(i);
+			Map tempMap =new HashMap();
+			tempMap.put("specificationName", temp.getSpecificationName());
+			//切割默认值组成数组
+			String[] defaultFields = temp.getSpecificationDefaultFields().split(",");
+			List<Map> specificationDefaultFields = new ArrayList<Map>();
+			for(int j=0; j < defaultFields.length; j++){
+				//获取默认规格参数，组成list
+				Map tempDefaultField =new HashMap();
+				tempDefaultField.put("specificationDefaultField", defaultFields[j]);
+				specificationDefaultFields.add(tempDefaultField);
+			}
+			tempMap.put("specificationDefaultFields", specificationDefaultFields);
+			List.add(tempMap);
+		}
+		String jsonStr = JSONArray.toJSONString(List);
+		this.outJson(response,jsonStr); 
 	}
 	
 		
